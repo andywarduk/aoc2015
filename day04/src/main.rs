21f75ab4b38863a75df8b06533641423581eaba8
Andy <andy.ward.uk@gmail.com>
2020-12-30 13:17:40 +0000

    Timings to day 4

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,17 +1,37 @@
-use std::sync::{Arc, atomic::{AtomicUsize, Ordering}};
+use std::{sync::{Arc, atomic::{AtomicUsize, Ordering}}, time::Instant};
 use std::thread;
+
+use md5::Digest;
+
+type CheckFn = fn(Digest) -> bool;
 
 fn main() {
     let key = "iwrupvqb";
 
-    println!("Number for part 1: {}", calc_hash_number5(key));
-    println!("Number for part 2: {}", calc_hash_number6(key));
+    println!("Number for part 1: {}", time_exec(calc_hash_number, key, check5));
+    println!("Number for part 2: {}", time_exec(calc_hash_number, key, check6));
+    println!("Number for part 1 ({} threads): {}", threads(), time_exec(calc_hash_number_threaded, key, check5));
+    println!("Number for part 2 ({} threads): {}", threads(), time_exec(calc_hash_number_threaded, key, check6));
 }
 
-fn calc_hash_number5(key: &str) -> u32 {
+fn time_exec(function: fn (key: &str, check: CheckFn) -> u32, key: &str, check: CheckFn) -> String {
+    let now = Instant::now();
+
+    let result = function(key, check);
+
+    let elapsed = now.elapsed();
+
+    format!("{} ({} secs)", result, elapsed.as_secs_f32())
+}
+
+fn threads() -> usize {
+    num_cpus::get()
+}
+
+fn calc_hash_number(key: &str, check: CheckFn) -> u32 {
     for n in 0.. {
         let digest = md5::compute(format!("{}{}", key, n));
-        if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
+        if check(digest) {
             return n;
         }
     }
@@ -19,24 +39,31 @@ fn calc_hash_number5(key: &str) -> u32 {
     0
 }
 
-const THREADS: usize = 8;
+fn check5(digest: Digest) -> bool{
+    digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0
+}
 
-fn calc_hash_number6(key: &str) -> u32 {
-    let mut threads = Vec::new();
+fn check6(digest: Digest) -> bool{
+    digest[0] == 0 && digest[1] == 0 && digest[2] == 0
+}
+
+fn calc_hash_number_threaded(key: &str, check: fn(Digest) -> bool) -> u32 {
+    let mut thread_handles = Vec::new();
     let result: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+    let tot_threads = threads();
 
     // Spawn threads
-    for thread in 0..THREADS {
+    for thread in 0..tot_threads {
         let thread_key = String::clone(&String::from(key));
         let thread_res = result.clone();
 
-        let handle = thread::spawn(move || calc_hash_number6_thread(thread, thread_key, thread_res));
+        let handle = thread::spawn(move || calc_hash_number_thread(thread, tot_threads, thread_key, thread_res, check));
 
-        threads.push(handle);
+        thread_handles.push(handle);
     }
 
     // Wait for threads
-    for t in threads {
+    for t in thread_handles {
         t.join().unwrap();
     }
 
@@ -44,11 +71,11 @@ fn calc_hash_number6(key: &str) -> u32 {
     result.load(Ordering::Relaxed) as u32
 }
 
-fn calc_hash_number6_thread(thread: usize, key: String, res: Arc<AtomicUsize>) {
-    for n in (thread..).step_by(THREADS) {
+fn calc_hash_number_thread(thread: usize, tot_threads: usize, key: String, res: Arc<AtomicUsize>, check: fn(Digest) -> bool) {
+    for n in (thread..).step_by(tot_threads) {
         let digest = md5::compute(format!("{}{}", key, n));
 
-        if digest[0] == 0 && digest[1] == 0 && digest[2] == 0 {
+        if check(digest) {
             // Found an answer
             let mut result = res.load(Ordering::Acquire);
 
@@ -79,6 +106,6 @@ fn test_md5() {
 
 #[test]
 fn test_calc_hash_number5() {
-    assert!(calc_hash_number5("abcdef") == 609043);
-    assert!(calc_hash_number5("pqrstuv") == 1048970);
+    assert!(calc_hash_number("abcdef", check5) == 609043);
+    assert!(calc_hash_number("pqrstuv", check5) == 1048970);
 }
